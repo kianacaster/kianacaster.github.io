0f2f07f0c9bb554bc9d91a5a4af0d62e1cc8166f
Kian Acaster <acasterkian@gmail.com>
2017-05-15 18:15:01 +0100

    Update sketch.js

--- bouncyball/sketch.js
+++ bouncyball/sketch.js
@@ -1,30 +1,50 @@
-var x = 180;
-var y = 180;
-var xspeed = 9;
-var yspeed = 10;
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+var width = 600;
+var height = 600;
+canvas.width = width;
+canvas.height = height;
+var speed = 10;
+var ball = new Ball(width / 2, height / 2);
 
-
-function setup() 
-{
-  createCanvas(600, 600);
+function Ball(x,y){
+	this.x = x;
+	this.y = y;
+	this.dx = 9;
+	this.dy = 10;
+	this.r = 20;
+	this.draw = function(){
+		c.fillStyle = "#FFFFFF";
+		c.beginPath();
+		c.arc(this.x,this.y,this.r,0,Math.PI*2);
+		c.fill();
+	}
+	
+	this.update = function(){
+		this.x += this.dx;
+		this.y += this.dy;
+	
+		if(this.x > width - this.r || this.x < this.r){
+			this.dx = invert(this.dx);
+		}
+		
+		if(this.y > height - this.r || this.y < this.r){
+			this.dy = invert(this.dy);
+		}
+	}
 }
 
-function draw() {
-	background(0);
-  	fill(255);
-  	ellipse(x, y, 32);
-	x = x + xspeed;
-	
-    if (x > 600 - 32 || x < 0 + 32)  {
-    	xspeed = -xspeed ;	
-  	}
-	
-	y = y + yspeed;
-	
+function animate(){
+	c.fillStyle = "#000000";
+	c.fillRect(0,0,width,height);
 
-  	if (y > 600 - 32 || y < 0 + 32) {
-    	yspeed = -yspeed;
-		  	
-  	}
-
-}
+	requestAnimationFrame(function(){
+		setTimeout(function(){
+			animate();
+		}, speed)}
+	);
+	
+	ball.draw();
+	ball.update();
+}	
+animate();	
