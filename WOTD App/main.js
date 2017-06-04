7517603c3584e7ce7ddd3047081616516c67db3c
Kian Acaster <acasterkian@gmail.com>
2017-06-04 18:01:05 +0100

    Add files via upload

---
+++ WOTD App/main.js
@@ -0,0 +1,32 @@
+var config = {
+	apiKey: "AIzaSyCWgWgpWEt9qjAV-2MBWyDOsQCUZ8Mwt04",
+	authDomain: "wotd-97c52.firebaseapp.com",
+	databaseURL: "https://wotd-97c52.firebaseio.com",
+	projectId: "wotd-97c52",
+	storageBucket: "wotd-97c52.appspot.com",
+	messagingSenderId: "308942664548"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+ref = database.ref('date');
+var d = new Date();
+var index;
+ref.on('value', function(data){
+	var vals = data.val();
+	var keys = Object.keys(vals);
+	var day = vals[keys[keys.length - 1]].day;
+	console.log(day);
+	
+	if(d.getDay() !== day){
+		ref.push({ 
+			day: d.getDay(),
+			index: Math.floor(Math.random() * words.length)
+		});
+	}
+	index = vals[keys[keys.length - 1]].index;
+	document.getElementById("word").innerHTML = words[index][0];
+	document.getElementById("def").innerHTML = words[index][1];
+}, function(error){
+	console.log(error);
+}
+);
