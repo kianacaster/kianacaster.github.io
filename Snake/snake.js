cfe8f1bd84c2d47ab86980a332a7688243b11b18
Kian Acaster <acasterkian@gmail.com>
2017-07-04 13:13:10 +0100

    Update snake.js

--- Snake/snake.js
+++ Snake/snake.js
@@ -62,7 +62,7 @@ function Snake(){
 	
 	this.show = function(){
 	noStroke();
-		fill(255);
+		fill(0);
 		for(var i = 0; i < this.tail.length; i++){
 			rect(this.tail[i].x, this.tail[i].y, scl, scl);
 		}
