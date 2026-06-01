fbec642c1a87fd66cdacd9915e68125db9b1e858
Kian Acaster <acasterkian@gmail.com>
2026-06-01 18:45:48 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,11 +47,6 @@ async function handleCommand(command) {
             break;
         case 'about':
             print(`
-My journey into computer science began in year four when my brother introduced me to the JavaScript <code>alert()</code> function in an online code sandbox. We spent hours making silly messages pop up on our grandad's laptop, and from that moment, I was hooked. The questions "How does this work?" and "What else can I make it do?" have been constant companions ever since.
-
-As a gaming enthusiast, I naturally gravitated toward creating browser-based games like Pong and Snake, which taught me the fundamentals of programming and game logic. Over time, I transitioned from using libraries like 'p5.js' to writing native JavaScript, exploring graphical projects and mathematical visualizations. 
-
-I am currently pursuing a degree in Computing Science at the University of Glasgow, where I am also studying Mathematics and Statistics. My academic journey has been shaped by a strong foundation in STEM subjects, including my prior GCSE level education at St. Peter's School in York, followed by Access to Higher Education courses in Maths and Physics provided by the University of Glasgow. Alongside my studies, I enjoy exploring new technologies, solving coding challenges, and working on creative projects that combine my love for problem-solving with my passion for design and innovation.
 `);
             break;
         case 'projects':
